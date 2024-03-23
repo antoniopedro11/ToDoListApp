292339df1fd50ef7d1e2f95aab35329f98ccded2
António Velez <avelez385@gmail.com>
2024-03-23 00:05:40 +0000

    Initial Commit

---
+++ ToDoListApp/ToDoListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListAppApp.swift
+//  ToDoListApp
+//
+//  Created by António Pedro Ramos Velez on 23/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
